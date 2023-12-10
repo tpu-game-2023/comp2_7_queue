d8895d687ac08312a1101cc04e5fd129fd134e92
origod <onigot0609@gmail.com>
2023-12-11 01:17:14 +0900

    テストを通しました

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -34,38 +34,69 @@ void finalize(QUEUE* q)
 // valの値をキューに入れる。実行の成否を返す
 bool enqueue(QUEUE* q, int val)
 {
+	if (q == NULL || countQueueableElements(q) == 0)return false;
+	*(q->tail) = val;
+	q->tail++;
+	if (q->tail == q->memory_end)
+		q->tail = q->memory_begin;
 	// ToDo: valのデータをキューに追加します
 	// 上手くいかない場合にはfalseを返します
 	// メモリを使い切ったら先頭アドレスに戻って追加して下さい
 
-	return false;
+	return true;
 }
 
 
 // addrから始まるnum個の整数をキューに入れる。実行の成否を返す
 bool enqueue_array(QUEUE* q, int* addr, int num)
 {
+	if (q == NULL || addr == NULL || num <= 0 || num > countQueueableElements(q))return false;
+
+	for (int i = 0; i < num; i++)
+	{
+		enqueue(q, addr[i]);
+	}
 	// ToDo: addrからnum個のデータをキューに追加します
 	// 上手くいかない場合にはfalseを返します
 	// メモリを使い切ったら先頭アドレスに戻って追加して下さい
 
-	return false;
+	return true;
 }
 
 // キューから一つの要素を取り出す(不具合時は0を返す)
 int dequeue(QUEUE* q)
 {
+	if (q == NULL || q->head == q->tail)return 0;
+
+	int data1st = *(q->head);
+	if (q->head == q->memory_end)
+		q->head = q->memory_begin;
 	// ToDo: 先頭のデータを返します
 
-	return 0;
+	return data1st;
 }
 
 // addrにキューからnumの要素を取り出す。取り出せた個数を返す
 int dequeue_array(QUEUE* q, int* addr, int num)
 {
+	if (q == NULL || addr == NULL || num <= 0)return 0;
 	// ToDo: 先頭からnum個のデータをaddrに格納します
+	int cntdata = 0;
+	for (int i = 0; i < num; i++)
+	{
+		if (q->head == q->tail)
+		{
+			break;
+		}
+		else
+		{
+			addr[i] = dequeue(q);
+			cntdata++;
+		}
+	}
 
-	return 0;
+
+	return cntdata;
 }
 
 // キューが空かどうかを調べる
@@ -89,7 +120,7 @@ int countQueuedElements(const QUEUE* q)
 	if (q == NULL || q->memory_begin == NULL) return 0;
 
 	int max_counts = getMaxCount(q);
-	return (q->head + max_counts - q->tail) % max_counts;
+	return (q->tail + max_counts - q->head) % max_counts;
 }
 
 // 挿入可能なデータ数を得る
