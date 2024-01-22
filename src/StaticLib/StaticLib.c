d934224f7190605eb4e52e58d9036a86b6deea48
diceroll5656 <kuni0521iku@icloud.com>
2024-01-22 15:21:53 +0900

    テスト通すことが出来ませんでした

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 #include "../include/lib_func.h"
+#include <wtypes.h>
 
 
 // mem_size の容量でキュー用のメモリを確保する
@@ -38,7 +39,13 @@ bool enqueue(QUEUE* q, int val)
 	// 上手くいかない場合にはfalseを返します
 	// メモリを使い切ったら先頭アドレスに戻って追加して下さい
 
-	return false;
+	if (q == NULL) return false;
+	if (q->tail + 1 == q->head) return false;
+	if (q->tail == q->memory_end - 1 && q->memory_begin)return false;
+
+	*q->tail = val;
+	if (++q->tail == q->memory_end)q->tail = q->memory_begin;
+	return true;
 }
 
 
@@ -49,25 +56,92 @@ bool enqueue_array(QUEUE* q, int* addr, int num)
 	// 上手くいかない場合にはfalseを返します
 	// メモリを使い切ったら先頭アドレスに戻って追加して下さい
 
-	return false;
-}
+	if(q==NULL) return false;
+	if (q->memory_end - q->memory_begin <= num) return false;
+	if (num <= 0)return false;
 
+	if (q->tail < q->head) {
+		if (q->head <= q->tail + num)return false;
+
+		if (q->memory_end <= q->tail + num)
+		{
+			//ループする
+			int num1 = q->memory_end - q->tail;
+			memcpy_s(q->tail, sizeof(int) * num1, addr, sizeof(int) * num1);
+			num -= num1;
+			memcpy_s(q->memory_begin, sizeof(int) * (q->head - q->memory_begin), addr + num1, sizeof(int) * num);
+		}
+		else {
+			memcpy_s(q->tail, sizeof(int) * (q->memory_end - q->tail), addr, sizeof(int) * num);
+			q->tail += num;
+		}
+	}
+	else {
+		//ループしないとheadに追いつけない
+		if (q->memory_end <= q->tail + num)
+		{
+			int num1 = q->memory_end - q->tail;
+
+			if (q->head <= q->memory_begin + num - num1)return false;
+
+			memcpy_s(q->tail, sizeof(int) * num1, addr, sizeof(int) * num1);
+			num -= num1;
+			memcpy_s(q->memory_begin, sizeof(int) * (q->head - q->memory_begin), addr + num1, sizeof(int) * num);
+			q->tail = q->memory_begin + num;
+		}
+		else {
+			memcpy_s(q->tail, sizeof(int) * (q->memory_end - q->tail), addr, sizeof(int) * num);
+			q->tail += num;
+		}
+	}
+
+	return true;
 // キューから一つの要素を取り出す(不具合時は0を返す)
-int dequeue(QUEUE* q)
+	int dequeue(QUEUE * q);
 {
+	if (q == NULL) return 0;
+	if (q->tail == q->head)return 0;
 	// ToDo: 先頭のデータを返します
 
-	return 0;
+	int ret = *q->head;
+	if (++q->head == q->memory_end)q->head = q->memory_begin;
+
+	return ret;
 }
 
 // addrにキューからnumの要素を取り出す。取り出せた個数を返す
 int dequeue_array(QUEUE* q, int* addr, int num)
 {
 	// ToDo: 先頭からnum個のデータをaddrに格納します
+	if(q==NULL ||addr==NULL)return 0;
+	if (num <= 0) return false;
+	if(q->head<=q- tail){
+		int n = q->tail - q->head;
+		if (num < n)n = num;
+		memcpy_s(addr, sizeof(int) * num, q->head, sizeof(int) * n);
+		q->head += n;
+		return n;
+	}
 
-	return 0;
-}
+	else {
+		int left = q->memory_end - q->head;
+		if (left <= num) {
+			memcpy_s(addr, sizeof(int) * num, q->head, sizeof(int) * left);
+			num -= left;
+			if (q->tail - q->memory_begin < num) {
+				num = q->tail - q->memory_begin;
+			}
+			memcpy_s(addr + left, sizeof(int) * num, q->memory_begin, sizeof(int) * num);
+			q->head = q->memory_begin + num;
 
+			return left + num;
+		}
+		else
+		{
+			memcpy_s(addr, sizeof(int) * num, q->head, sizeof(int) * num);
+			q->head += num;
+		}
+	}
 // キューが空かどうかを調べる
 bool isEmpty(const QUEUE* q)
 {
